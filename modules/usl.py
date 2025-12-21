1648a95253087fa50cf301c089703b13a1589e01
Charlie <charlie@powerpcfan.xyz>
2025-12-21 17:28:47 -0500

    🧪 fix: fix bugs in USL checking code

--- modules/usl.py
+++ modules/usl.py
@@ -1,18 +1,41 @@
 import requests
+import time
 from .logger import logger
 
 
 def is_on_usl(username: str) -> bool:
-    url = "https://api.reddit.com/r/UniversalScammerList/wiki/database/{username}.json"
-
     username = username.lower()
+    url = f"https://corsproxy.io/?url=api.reddit.com/r/UniversalScammerList/wiki/database/{username}.json"
 
-    response = requests.get(url.format(username=username))
+    try:
+        response = requests.get(url)
+    except requests.RequestException as e:
+        logger.warning(f"Error checking USL for {username}: {e}. Treating as on USL.")
+        return True  # Assume user is on USL
 
-    if response.ok:
+    retries = 0
+    while response.status_code == 429 and retries < 3:
+        fallback = 5
+
+        retry_after = int(response.headers.get("Retry-After", fallback))
+
+        if retry_after > 30:
+            retry_after = fallback
+
+        time.sleep(retry_after)
+
+        try:
+            response = requests.get(url)
+        except requests.RequestException as e:
+            logger.warning(f"Error checking USL for {username}: {e}. Treating as on USL.")
+            return True  # Assume user is on USL
+
+        retries += 1
+
+    if response.status_code == 200:
         return True  # User is on the USL
     elif response.status_code == 404:
         return False  # User is not on the USL
     else:
-        logger.warning(f"Unexpected response from USL API for user u/{username}: {response.status_code}.")  # noqa: E501
-        return False  # Unrecognized response, treat as on USL
+        logger.warning(f"Unexpected status code from USL API for user u/{username}: {response.status_code}. Treating as on USL.")  # noqa: E501
+        return True  # Unrecognized response, treat as on USL
